702f0929a169d1e9617ad51fa6411412be44246a
Supriya Rai <88661supriyak@gmail.com>
2026-06-26 22:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (53.16%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+      if(num <= 3 ) return true;
+
+      long long low = 1 ;
+      long long high = num/2;
+
+      while(low <= high)
+      {
+         long long mid = (low + high)/2;
+
+         if(mid*mid == num) return true;
+         else if(mid*mid > num) high = mid-1;
+         else low = mid+1;
+      }
+      return false;
+    }
+};
